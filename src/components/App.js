5bf2001bb4f66ccb8928fdaa4e667a023784b0f7
Oladele-israel <omoleoladele43@gmail.com>
2024-01-25 13:26:09 +0100

    displayin user input in ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,52 @@
+import { useState } from "react";
+
 function App(){
+    const [ description, setDescription] = useState('');
+    const [tasks, setTasks] = useState([]);
+    
+    function handleAddItems(task){
+        setTasks([...tasks, task]);
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+    
+        if(!description) return;
+        const newTask = {description, done: false, id:Date.now()}
+
+        handleAddItems(newTask)
+        setDescription('');
+   }
+
     return(
         <>
     <div className="header">
-        <div className="logo">TaskADD📑✏🗓</div>
+        <div className="logo">TODO</div>
         <div className="addMenu">
-            <input type="text" placeholder="enter task"/>
-            <button>Add+</button>
+            <form onSubmit={handleSubmit}>
+             <input type="text" placeholder="Enter Task" value={description} onChange={(e)=>setDescription(e.target.value)}/>
+             <button type="submit">+</button>
+            </form>
         </div>
     </div>
 
-    <Main/>
+    <Main tasks={tasks} />
     </>
     )
 }
 
-function Main(){
+
+function Main({tasks}){
     return(
         <section>
-           <div className="task-container">
-
-            </div>
-            <span className="plus">+</span>
+            {
+                tasks.map((taskItem)=>{
+                    return <div className="task-container" key={taskItem.id}>
+                          <input type="checkbox"/>
+                     <span >{taskItem.description}  </span>
+                    </div>
+                })
+            }
         </section>
     )
 }
